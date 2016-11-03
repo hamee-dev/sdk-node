4ff2edc7dc2d9e5f1acce7999e5704cfc9ef2e7c
Leko <leko.noor@gmail.com>
2016-11-03 22:01:04 +0900

    test: Add pending test cases

--- test/Query.spec.js
+++ test/Query.spec.js
@@ -1,4 +1,56 @@
 
 describe('Query', () => {
-  
+  describe('get DEFAULT_LIMIT', () => {
+    it('must be 10000')
+  })
+
+  describe('count', () => {
+    it('must return Promise')
+    it('must pass integer if Promise fulfilled')
+  })
+
+  describe('get', () => {
+    it('must return Promise')
+    it('must pass unwrapped Object if Promise fulfilled')
+  })
+
+  describe('getInBatches', () => {
+    it('must return Promise')
+    it('must call iteratee if response not empty')
+  })
+
+  describe('limit', () => {
+    it('must return fresh instance of Query')
+    it('must have limit option')
+  })
+
+  describe('offset', () => {
+    it('must return fresh instance of Query')
+    it('must have offset option')
+  })
+
+  describe('where', () => {
+    it('must return fresh instance of Query')
+    it('must inherit exists conditions')
+  })
+
+  describe('fetch', () => {
+    it('must return Promise')
+  })
+
+  describe('request', () => {
+    it('must return Promise')
+  })
+
+  describe('toOperator', () => {
+    it('must return NE-operator if mapping exists')
+    it('must throw Error if mapping not exists')
+  })
+
+  describe('toParameter', () => {
+    it('must return Object')
+    it('must have limit if limit present')
+    it('must have offset if offset present')
+    it('must have all filter parameters')
+  })
 })
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,4 +1,44 @@
 
 describe('Nextengine', () => {
-  
+  describe('request', () => {
+    it('must return Promise')
+  })
+
+  describe('getConnection', () => {
+    it('must return instance of Connection')
+  })
+
+  describe('query', () => {
+    it('must return instance of Query')
+  })
+
+  describe('create', () => {
+    it('must return Promise')
+  })
+
+  describe('update', () => {
+    it('must return Promise')
+  })
+
+  describe('upload', () => {
+    it('must return Promise')
+  })
+
+  describe('waitFor', () => {
+    it('must return Promise')
+  })
+
+  describe('uploadAndWaitFor', () => {
+    it('must return Promise')
+  })
+
+  describe('authorize', () => {
+    it('must return Promise')
+  })
+
+  describe('getAuthorizeURL', () => {
+    it('must return url with query string client_id')
+    it('must return url with query string client_secret')
+    it('must return url with query string redirect_uri if redirectUri present')
+  })
 })
