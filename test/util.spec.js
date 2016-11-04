2f545041409fb49d1419fd156af0fa944a7d9a28
Leko <leko.noor@gmail.com>
2016-11-04 23:08:45 +0900

    chore: Add util test cases

--- test/util.spec.js
+++ test/util.spec.js
@@ -1,7 +1,15 @@
 /* eslint-env mocha */
 
+const assert = require('assert')
+const { digg } = require('../lib/util')
+
 describe('util', () => {
   describe('digg', () => {
-    it('must call object-path.get')
+    it('must call object-path.get', () => {
+      const expected = 100
+      const fn = digg('a.b.c')
+
+      assert.equal(fn({ a: { b: { c: expected } } }), expected)
+    })
   })
 })
