d56d3e917964c30ffb954d75e5da2841ecef6f7f
Leko <leko.noor@gmail.com>
2016-11-05 21:05:19 +0900

    test: Add Path test cases

--- test/Path.spec.js
+++ test/Path.spec.js
@@ -1,12 +1,22 @@
 /* eslint-env mocha */
 
+const assert = require('assert')
+const Path = require('../lib/Path')
+const { ReceiveOrder } = require('../Entity')
+
 describe('Path', () => {
   describe('resolve', () => {
-    it('must return path string if string passed')
-    it('must return path string if Entity passed')
+    it('must return path string if string passed', () => {
+      assert.equal(Path.resolve('hoge', 'search'), '/api_v1_hoge/search')
+    })
+    it('must return path string if Entity passed', () => {
+      assert.equal(Path.resolve(ReceiveOrder, 'search'), '/api_v1_receiveorder_base/search')
+    })
   })
 
   describe('findPath', () => {
-    it('must return string')
+    it('must return string', () => {
+      assert.equal(typeof Path.findPath(), 'string')
+    })
   })
 })
