936761b91f8e8c0ac3d7b40d0becd831f510a7dc
Shingo Inoue <inoue.shingo@hamee.co.jp>
2016-11-06 18:24:40 +0900

    feat: Add doc comment

--- Entity/UploadQueue.js
+++ Entity/UploadQueue.js
@@ -1,6 +1,18 @@
 const Entity = require('./Entity')
 
 class UploadQueue extends Entity {
+  static get COMPLETED () {
+    return 2
+  }
+
+  static get FAILED () {
+    return -1
+  }
+
+  static get IN_PROGRESS () {
+    return 1
+  }
+
   static get getAsInfo () {
     return false
   }
